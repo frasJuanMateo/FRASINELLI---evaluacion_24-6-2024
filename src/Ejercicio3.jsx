d9cd42c15e3041aeffd25cc50916d9336cef10fc
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-06-24 16:44:35 -0300

    Se corrigio el ejercicio 3

--- src/Ejercicio3.jsx
+++ src/Ejercicio3.jsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import {useState} from "react";
 
 function ComponenteLista(props) {
   const lista = ["Informatica Aplicada II", "Programacion II", "Robotica", "Matematica", "Fisica"]
   return (<div>
-      <h1>{props.mensaje}</h1>
       {lista.map((item) => <li>{item}</li>)}
+      <h1>{props.value}</h1>
   </div>)
 }
 
 export default function Ejercicio3() {
-  const [mensaje, setMensaje] = useState("")
-  const handleChange = (event) => {
-    setMensaje(event.target.value)
-  }
   return (
     <div>
       <h2>Ejercicio 3: Modularización</h2>
       <p>Algunas materias de quinto año:</p>
       <ul>
-        <ComponenteLista mensaje={mensaje}/>
-        <input type="text" onChange={handleChange} />
+        <ComponenteLista value="Inserte mensaje aqui"/>
       </ul>
     </div>
   )
