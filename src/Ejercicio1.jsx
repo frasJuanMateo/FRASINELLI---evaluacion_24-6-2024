301f23ce9a212c2cf69a7256130bbf34666a8eb6
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-06-24 15:42:40 -0300

    Se corrigio ejercicio 1

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 export default function Ejercicio1() {
-  const [fruta, selectFruta] = useState("manzana")
+  const [fruta, selectFruta] = useState("Manzana")
   const handleFruitChange = (e) => {
     selectFruta(e.target.value)
   }
@@ -11,10 +11,10 @@ export default function Ejercicio1() {
 
       <label>Seleccione una fruta: </label>
       <select name="frutas" onChange={handleFruitChange}>
-        <option value="manzana">Manzana</option>
-        <option value="pera">Pera</option>
-        <option value="naranja">Naranja</option>
-        <option value="mandarina">Mandarina</option>
+        <option value="Manzana">Manzana</option>
+        <option value="Pera">Pera</option>
+        <option value="Naranja">Naranja</option>
+        <option value="Mandarina">Mandarina</option>
       </select>
       <p>Seleccionaste: <b>{fruta}</b></p>
     </div>
