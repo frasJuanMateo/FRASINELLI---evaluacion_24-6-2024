793f52540179445ab4d163535e7c3d5accd7a8da
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-06-24 16:39:38 -0300

    Se realizo el ejercicio 3

--- src/Ejercicio3.jsx
+++ src/Ejercicio3.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {useState} from "react";
+
+function ComponenteLista(props) {
+  const lista = ["Informatica Aplicada II", "Programacion II", "Robotica", "Matematica", "Fisica"]
+  return (<div>
+      <h1>{props.mensaje}</h1>
+      {lista.map((item) => <li>{item}</li>)}
+  </div>)
+}
 
 export default function Ejercicio3() {
+  const [mensaje, setMensaje] = useState("")
+  const handleChange = (event) => {
+    setMensaje(event.target.value)
+  }
   return (
     <div>
       <h2>Ejercicio 3: Modularización</h2>
       <p>Algunas materias de quinto año:</p>
       <ul>
-        {/* lista de materias */}
+        <ComponenteLista mensaje={mensaje}/>
+        <input type="text" onChange={handleChange} />
       </ul>
     </div>
   )
