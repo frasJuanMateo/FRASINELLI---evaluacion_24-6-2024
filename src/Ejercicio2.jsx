c94c32d0d4462e55b4e810463f517323c35c3d1b
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-06-24 16:08:15 -0300

    Se hizo ejercicio 2

--- src/Ejercicio2.jsx
+++ src/Ejercicio2.jsx
@@ -1,9 +1,33 @@
+import React, {useState} from "react";
+
 export default function Ejercicio2() {
-  
+  const [formData, setFormData] = useState({
+    nombre: "-",
+    edad: "-"
+  });
+
+  const handleSubmit = (event) => {}
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
   return (
     <div>
       <h2>Ejercicio 2: Formulario</h2>
-      {/* Crear formulario */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Nombre:</label>
+        <input name="nombre" type="text" onChange={handleChange}/>
+        <label htmlFor="">Edad:</label>
+        <input name="edad" type="number" onChange={handleChange}/>
+        <button type="submit">Subir Info.</button>
+      </form>
+      <p><b>{formData.nombre}</b> tiene <b>{formData.edad}</b> años.</p>
     </div>
   )
 }
