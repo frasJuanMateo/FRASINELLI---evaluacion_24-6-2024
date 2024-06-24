735ba28c9bd60eeaa4c1c20467cd877333fb0758
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-06-24 15:40:21 -0300

    Se termino ejercicio 1

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,12 +1,22 @@
-export default function Ejercicio1() {
+import React, {useState} from "react";
 
+export default function Ejercicio1() {
+  const [fruta, selectFruta] = useState("manzana")
+  const handleFruitChange = (e) => {
+    selectFruta(e.target.value)
+  }
   return (
     <div>
       <h2>Ejercicio 1: Lista desplegable</h2>
 
       <label>Seleccione una fruta: </label>
-      {/* Crear lista de selección */}
-
+      <select name="frutas" onChange={handleFruitChange}>
+        <option value="manzana">Manzana</option>
+        <option value="pera">Pera</option>
+        <option value="naranja">Naranja</option>
+        <option value="mandarina">Mandarina</option>
+      </select>
+      <p>Seleccionaste: <b>{fruta}</b></p>
     </div>
   )
 }
